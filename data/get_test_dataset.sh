4b1cba5682af7e84d4422caddfe15abca338d1cb
NeuralVFX <vfxhoneybadger@gmail.com>
2018-07-17 17:25:25 +0545

    fixing comment

--- data/get_test_dataset.sh
+++ data/get_test_dataset.sh
@@ -3,4 +3,5 @@
 URL=https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2shoes.tar.gz
 TAR_FILE=./data/edges2shoes.tar.gz
 wget $URL -O $TAR_FILE
-tar -xvzf TAR_FILE
+tar -xvzf $TAR_FILE -C ./data/
+
