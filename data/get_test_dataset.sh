96705d50c42266cd2170fe4ed2958ed90fa02e0c
NeuralVFX <vfxhoneybadger@gmail.com>
2018-07-17 16:49:44 +0545

    updating sh script
    fixing save and load state to include test losses

--- data/get_test_dataset.sh
+++ data/get_test_dataset.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 URL=https://people.eecs.berkeley.edu/~tinghuiz/projects/pix2pix/datasets/edges2shoes.tar.gz
-wget $URL
-tar -xvzf edges2shoes.tar.gz
+TAR_FILE=./data/edges2shoes.tar.gz
+wget $URL -O $TAR_FILE
+tar -xvzf TAR_FILE
